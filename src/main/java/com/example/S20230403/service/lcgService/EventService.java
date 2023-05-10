a6b440a44833d6d418e916b8003dff28bb19d8f8
User <User@DESKTOP-JFUABTI>
2023-05-10 10:49:13 +0900

    찬규 2023.5.10 오전 10:48분 머지_이벤트 페이지 추가

---
+++ src/main/java/com/example/S20230403/service/lcgService/EventService.java
@@ -0,0 +1,11 @@
+package com.example.S20230403.service.lcgService;
+
+import java.util.List;
+
+import com.example.S20230403.model.Event;
+
+public interface EventService {
+
+	List<Event> getEvent();
+
+}
